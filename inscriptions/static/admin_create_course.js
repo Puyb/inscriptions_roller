2ebbe5c7cb3d7a6a768fd08796f47d8a942ba99f
Puyb <puyb@puyb.net>
2015-12-23 02:19:55 +0100

    fix prices when error in course creation

--- inscriptions/static/admin_create_course.js
+++ inscriptions/static/admin_create_course.js
@@ -3,6 +3,7 @@ $(function() {
     $.ajax('/static/course_models.json', {
         success: function(r) {
             COURSE_MODEL = r;
+            generate_prices($('[name=course_model]:checked').val());
         }
     });
     var $place = $('<div>');
@@ -12,8 +13,9 @@ $(function() {
             .append($place)
         );
 
-    $('[name=course_model]').click(function() {
-        var model = COURSE_MODEL[this.value];
+    function generate_prices(model_name) {
+        if (!model_name) return;
+        var model = COURSE_MODEL[model_name];
 
         var $table = $('<table>')
             .append($('<tr>')
@@ -22,13 +24,14 @@ $(function() {
                 .append($('<td>').html('Prix standard'))
                 .append($('<td>').html('Prix augmenté'))
             );
+        previous = JSON.parse($('[name=course_prix]').val() || '{}');
         $place.html($table);
         model.categories.forEach(function(c) {
             $('<tr>')
                 .append($('<th>').html(c.code))
                 .append($('<td>').html(c.nom))
-                .append($('<td>').append($('<input>').attr('name', c.code + '_prix1').val(c.prix1)))    
-                .append($('<td>').append($('<input>').attr('name', c.code + '_prix2').val(c.prix2)))    
+                .append($('<td>').append($('<input>').attr('name', c.code + '_prix1').val(c.code in previous ? previous[c.code].prix1 : c.prix1)))    
+                .append($('<td>').append($('<input>').attr('name', c.code + '_prix2').val(c.code in previous ? previous[c.code].prix2 : c.prix2)))    
                 .appendTo($table);
         });
         $table.find('input').change(function() {
@@ -40,5 +43,8 @@ $(function() {
             });
             $('[name=course_prix]').val(JSON.stringify(d));
         });
+    }
+    $('[name=course_model]').click(function() {
+        generate_prices(this.value);
     });
 });
