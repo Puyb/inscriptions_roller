0980fe107acb07d46333cdffae9eee16470cb3c8
Puyb <puyb@puyb.net>
2016-07-04 09:11:11 +0200

    challenge

---
+++ inscriptions/static/admin_create_challenge.js
@@ -0,0 +1,37 @@
+$(function() {
+    COURSE_MODEL = {};
+    $.ajax('/static/course_models.json', {
+        success: function(r) {
+            COURSE_MODEL = r;
+            generate_cat($('[name=course_model]:checked').val());
+        }
+    });
+    var $place = $('<div>');
+    $('fieldset')
+        .append($('<div class="form-row">')
+            .append($('<label class="required">').html('Catégories&nbsp;:'))
+            .append($place)
+        );
+
+    function generate_cat(model_name) {
+        if (!model_name) return;
+        var model = COURSE_MODEL[model_name];
+
+        var $table = $('<table>')
+            .append($('<tr>')
+                .append($('<th>').html('Code'))
+                .append($('<td>').html('Nom'))
+            );
+        $place.html($table);
+        if(model.categories)
+            model.categories.forEach(function(c) {
+                $('<tr>')
+                    .append($('<th>').html(c.code))
+                    .append($('<td>').html(c.nom))
+                    .appendTo($table);
+            });
+    }
+    $('[name=course_model]').click(function() {
+        generate_cat(this.value);
+    });
+});
