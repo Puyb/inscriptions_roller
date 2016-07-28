5f8f94035da580d234dd3b1a34f65af4be6b3e8c
Puyb <puyb@puyb.net>
2016-07-28 10:18:34 +0200

    missing migration

---
+++ inscriptions/migrations/0021_auto_20160728_1018.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inscriptions', '0020_auto_20160705_1733'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='challenge',
+            name='courses',
+            field=models.ManyToManyField(null=True, related_name='challenges', blank=True, to='inscriptions.Course'),
+        ),
+        migrations.AlterField(
+            model_name='equipechallenge',
+            name='equipe',
+            field=models.ForeignKey(related_name='challenges', to='inscriptions.Equipe'),
+        ),
+    ]
